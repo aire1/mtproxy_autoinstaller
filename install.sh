49c7bf7205397b38839506a07a7675e071d716ca
aire1 <31969697+aire1@users.noreply.github.com>
2018-07-03 20:22:33 +0300

    First attempt

---
+++ install.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+ABSOLUTE_FILENAME=`readlink -e "$0"`
+DIRECTORY=`dirname "$ABSOLUTE_FILENAME"`
+IP=`wget -qO- eth0.me`
+INSTALL_ROOT="/opt/mtprotoproxy"
+gitlink="https://github.com/alexbers/mtprotoproxy.git"
+
+preinstall() {
+#downloading
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install htop git
+
+#ports
+sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 1443
+sudo apt-get install iptables-persistent
+sudo service netfilter-persistent save
+
+echo > check_file.cfg
+install
+}
+
+if [ -e $DIRECTORY/check_file.cfg ]; then 
+install; else
+preinstall
+fi
+
+install() {
+if grep "MTProxy" check_file.cfg; then
+echo "MTProxy уже установлен на вашем сервере! Установка отменена (для сброса данных о установке введите команду: rm check_file.cfg)"
+exit 1
+fi
+
+usage() {
+    echo "Использование: ./install.sh -s <secret>"
+}
+SECRET=`head -c 16 /dev/urandom | xxd -ps`
+
+while getopts "s:" arg; do
+    case $arg in
+        s)
+            SECRET=$OPTARG
+            ;;
+        *)
+            usage
+            exit 1
+    esac
+done
+
+if [ -z `echo $SECRET | grep -x '[[:xdigit:]]\{32\}'` ]; then
+    echo "Secret должен быть 32-значным ключом, содержащим только HEX-символы"
+    exit 1
+fi
+
+CONFIG="PORT = 1443
+USERS = {\"tg\":  \"${SECRET}\"}"
+
+#MTProxy setup
+cd /opt
+sudo mkdir $INSTALL_ROOT
+sudo chown $USER $INSTALL_ROOT
+git clone -b master $gitlink
+cd $INSTALL_ROOT
+rm config.py
+echo | sed  "i$CONFIG" > config.py
+sudo cp $DIRECTORY/MTProxy.service /etc/systemd/system/
+sudo systemctl daemon-reload && sudo systemctl restart MTProxy.service && sudo systemctl enable MTProxy.service
+finish
+}
+
+socks_install() {
+read -p "Желаете установить SOCKS5? (y/n)" check
+if [[check != "y"]]; then
+exit 0
+else
+./socks_install.sh
+}
+
+finish() {
+cd $DIRECTORY
+echo "MTProxy " > check_file.cfg
+echo "Установка MTProxy успешно завершена! Ваша ссылка для подключения: https://t.me/proxy?server=${IP}&port=443&secret=${SECRET}"
+echo "IP: ${IP}, port: 443 или 1443, secret: ${SECRET}"
+if grep "SOCKS5" check_file.cfg; then
+exit 0
+else
+socks_install
+fi
+}
---
+++ socks_install.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+ABSOLUTE_FILENAME=`readlink -e "$0"`
+DIRECTORY=`dirname "$ABSOLUTE_FILENAME"`
+IP=`wget -qO- eth0.me`
+INSTALL_ROOT="/opt/tgsocksproxy"
+gitlink="https://github.com/alexbers/tgsocksproxy.git"
+
+preinstall() {
+#downloading
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install htop git
+
+echo > check_file.cfg
+install
+}
+
+if [ -e $DIRECTORY/check_file.cfg ]; then 
+install; else
+preinstall
+fi
+
+install() {
+if grep "SOCKS" check_file.cfg; then
+echo "SOCKS5 уже установлен на вашем сервере! Установка отменена (для сброса данных о установке введите команду: rm check_file.cfg)"
+exit 1
+fi
+
+usage() {
+    echo "Использование: ./installsocks.sh -l <login> -p <password>"
+}
+LOGIN=`tr -dc A-Za-z0-9 < /dev/urandom | head -c 5 | xargs`
+PASSWORD=`tr -dc A-Za-z0-9 < /dev/urandom | head -c 7 | xargs`
+
+while getopts "l:p:" arg; do
+	case $arg in
+		l)
+		LOGIN=$OPTARG
+        ;;
+		p)
+		PASSWORD=$OPTARG
+		;;
+		*)
+		usage
+		exit 1
+	esac
+done
+
+CONFIG="PORT = 1080
+USERS = {\"${LOGIN}\": \"${PASSWORD}\"}"
+
+#SOCKS5 setup
+cd /opt
+sudo mkdir $INSTALL_ROOT
+sudo chown $USER $INSTALL_ROOT
+git clone $gitlink
+cd $INSTALL_ROOT
+rm config.py
+echo | sed  "i$CONFIG" > config.py
+sudo cp $DIRECTORY/SOCKS5.service /etc/systemd/system/
+sudo systemctl daemon-reload && sudo systemctl restart SOCKS5.service && sudo systemctl enable SOCKS5.service
+finish
+}
+
+mtproxy_install() {
+read -p "Желаете установить MTProxy? (y/n)" check
+if [[check != "y"]]; then
+exit 0
+else
+./install.sh
+}
+
+finish() {
+cd $DIRECTORY
+echo "SOCKS5 " > check_file.cfg
+echo "Установка SOCKS5 успешно завершена! Ваша ссылка для подключения: https://t.me/socks?server=${IP}&port=1080&user=${LOGIN}&pass=${PASSWORD}"
+echo "IP: ${IP}, port: 1080, login: ${LOGIN}, password: ${PASSWORD}"
+if grep "MTProxy" check_file.cfg; then
+exit 0
+else
+mtproxy_install
+fi
+}
