c6861a71c7e98b932f83496d2aa5609d2495c2b1
aire1 <31969697+aire1@users.noreply.github.com>
2018-07-04 15:53:35 +0300

    more readable

--- socks_install.sh
+++ socks_install.sh
@@ -8,31 +8,49 @@ LOGIN=$1
 PASSWORD=$2
 
 checkinstallation() {
+
 if grep -q "SOCKS" check_file.cfg; then
+
 echo "SOCKS5 уже установлен на вашем сервере. Установка отменена (для сброса данных о установке введите команду: rm check_file.cfg)"
+
 exit 1
+
 fi
 }
 
 finish() {
+
 cd $DIRECTORY
+
 echo "SOCKS " > check_file.cfg
+
 echo "Установка SOCKS5 успешно завершена! Ваша ссылка для подключения: https://t.me/socks?server=${IP}&port=1080&user=${LOGIN}&pass=${PASSWORD}"
+
 }
 
 generate() {
+
 if [ -n "$LOGIN" ]
+
 then
+
 LOGIN=$LOGIN
+
 else
+
 LOGIN=`tr -dc A-Za-z0-9 < /dev/urandom | head -c 5 | xargs`
 fi
 
 if [ -n "$PASSWORD" ]
+
 then
+
 PASSWORD=$PASSWORD
+
 else
+
 PASSWORD=`tr -dc A-Za-z0-9 < /dev/urandom | head -c 7 | xargs`
+
 fi
 }
 
@@ -43,34 +61,51 @@ generate
 CONFIG="PORT = 1080\nUSERS = {\"${LOGIN}\": \"${PASSWORD}\"}"
 
 writing(){
+
 cd $INSTALL_ROOT
+
 rm config.py
+
 echo | sed  "i$CONFIG" > config.py
 }
 
 #SOCKS5 setup
+
 cd /opt
+
 sudo mkdir $INSTALL_ROOT
+
 sudo chown $USER $INSTALL_ROOT
+
 git clone $gitlink
+
 writing
+
 sudo cp $DIRECTORY/SOCKS5.service /etc/systemd/system/
+
 sudo systemctl daemon-reload && sudo systemctl restart SOCKS5.service && sudo systemctl enable SOCKS5.service
+
 finish
 }
 
 preinstall() {
 #downloading
+
 sudo apt-get update && sudo apt-get upgrade
+
 sudo apt-get install htop git
 
 echo > check_file.cfg
+
 install
 }
 
 checkinstallation
 
 if [ -e $DIRECTORY/check_file.cfg ]; then 
+
 install; else
+
 preinstall
+
 fi
